b5ac085a81403a58bf852ed335f67f6e11675c6e
Simone <simofoti@gmail.com>
2022-09-23 16:05:24 +0100

    Update installation script for installation with cuda 11.3

--- install_env.sh
+++ install_env.sh
@@ -3,23 +3,22 @@
 virtualenv -p python3 ./id-generator-env
 source ./id-generator-env/bin/activate
 
-export CUDA=cu101
+export PYTHON_V=38
+export CUDA=cu113
+export TORCH=1.12.1
+export TORCHVISION=0.13.1
+export TORCH_GEOM_VERSION=1.12.0
+
+export TORCH_3D_VERSION="${TORCH_GEOM_VERSION//.}"
 
 pip install cmake
 pip install trimesh pyrender tqdm matplotlib rtree openmesh tb-nightly av seaborn joypy menpo
 
-pip install torch==1.7.1+${CUDA} torchvision==0.8.2+${CUDA} -f https://download.pytorch.org/whl/torch_stable.html
+# install pytorch, pytorch geometric, and pytorch3d. make sure the correct variables were exported.
+pip3 install torch==${TORCH} torchvision==${TORCHVISION} --default-timeout=1000 --extra-index-url https://download.pytorch.org/whl/${CUDA}
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-${TORCH_GEOM_VERSION}+${CUDA}.html
+pip install pytorch3d==0.6.0  -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py${PYTHON_V}_${CUDA}_pyt${TORCH_3D_VERSION}/download.html
 
-# install pytorch geometric
-export TORCH=1.7.1
-pip install torch-scatter==2.0.6 -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-sparse==0.6.9 -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-cluster==1.5.9 -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-spline-conv==1.2.1 -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-geometric==$TORCH
-
-export PYTHON_V=38
-pip install pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py${PYTHON_V}_${CUDA}_pyt171/download.html
 pip install geomloss pykeops
 
 # for demo in notebook
