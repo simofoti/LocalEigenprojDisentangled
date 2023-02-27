2af0cca4de953a960e48bcf4976f4df611f143d1
Simone <simofoti@gmail.com>
2023-02-27 15:34:33 +0000

    Refactor data loading

--- data_generation_and_loading.py
+++ data_generation_and_loading.py
@@ -87,19 +87,20 @@ class BodyGenerator(DataGenerator):
         t.export(os.path.join(self._data_dir, 'template.ply'))
 
 
-def get_data_loaders(config, template=None):
+def get_data_loaders(config, template=None, in_memory=True):
     data_config = config['data']
     batch_size = config['optimization']['batch_size']
 
-    train_set = MeshInMemoryDataset(
+    dataset = MeshInMemoryDataset if in_memory else MeshDataset
+    train_set = dataset(
         data_config['dataset_path'], dataset_type='train',
         precomputed_storage_path=data_config['precomputed_path'],
         normalize=data_config['normalize_data'], template=template)
-    validation_set = MeshInMemoryDataset(
+    validation_set = dataset(
         data_config['dataset_path'], dataset_type='val',
         precomputed_storage_path=data_config['precomputed_path'],
         normalize=data_config['normalize_data'], template=template)
-    test_set = MeshInMemoryDataset(
+    test_set = dataset(
         data_config['dataset_path'], dataset_type='test',
         precomputed_storage_path=data_config['precomputed_path'],
         normalize=data_config['normalize_data'], template=template)
@@ -151,10 +152,9 @@ class MeshCollater:
         return batched_data
 
 
-class MeshDataset(Dataset):
-    def __init__(self, root, precomputed_storage_path='precomputed',
-                 dataset_type='train', normalize=True,
-                 transform=None, pre_transform=None, template=None):
+class MixinMeshDataset:
+    def _initialize(self, root, precomputed_storage_path='precomputed',
+                    dataset_type='train', normalize=True, template=None):
         self._root = root
         self._precomputed_storage_path = precomputed_storage_path
         if not os.path.isdir(precomputed_storage_path):
@@ -173,175 +173,10 @@ class MeshDataset(Dataset):
         self._normalization_dict = normalization_dict
         self.mean = normalization_dict['mean']
         self.std = normalization_dict['std']
-        super(MeshDataset, self).__init__(root, transform, pre_transform)
-
-    @property
-    def raw_file_names(self):
-        if self._dataset_type == 'train':
-            file_names = self._train_names
-        elif self._dataset_type == 'test':
-            file_names = self._test_names
-        elif self._dataset_type == 'val':
-            file_names = self._val_names
-        else:
-            raise Exception("train, val and test are supported dataset types")
-        return file_names
-
-    @property
-    def processed_file_names(self):
-        return self._processed_files
 
     @property
     def normalization_dict(self):
         return self._normalization_dict
-
-    def download(self):
-        pass
-
-    def find_filenames(self):
-        files = []
-        for dirpath, _, fnames in os.walk(self._root):
-            for f in fnames:
-                if f.endswith('.ply') or f.endswith('.obj'):
-                    files.append(f)
-        return files
-
-    def split_data(self, data_split_list_path):
-        try:
-            with open(data_split_list_path, 'r') as fp:
-                data = json.load(fp)
-            train_list = data['train']
-            test_list = data['test']
-            val_list = data['val']
-        except FileNotFoundError:
-            all_file_names = self.find_filenames()
-            all_file_names.sort()
-
-            train_list, test_list, val_list = [], [], []
-            for i, fname in enumerate(all_file_names):
-                if i % 100 <= 5:
-                    test_list.append(fname)
-                elif i % 100 <= 10:
-                    val_list.append(fname)
-                else:
-                    train_list.append(fname)
-
-            data = {'train': train_list, 'test': test_list, 'val': val_list}
-            with open(data_split_list_path, 'w') as fp:
-                json.dump(data, fp)
-        return train_list, test_list, val_list
-
-    def load_mesh(self, filename):
-        mesh_path = os.path.join(self._root, filename)
-        mesh = trimesh.load_mesh(mesh_path, process=False)
-        mesh_verts = torch.tensor(mesh.vertices, dtype=torch.float,
-                                  requires_grad=False)
-        return mesh_verts
-
-    def compute_mean_and_std(self):
-        normalization_dict_path = os.path.join(
-            self._precomputed_storage_path, 'norm.pt')
-        try:
-            normalization_dict = torch.load(normalization_dict_path)
-        except FileNotFoundError:
-            assert self._dataset_type == 'train'
-            train_verts = None
-            for i, fname in tqdm.tqdm(enumerate(self._train_names)):
-                mesh_verts = self.load_mesh(fname)
-                if i == 0:
-                    train_verts = torch.zeros(
-                        [len(self._train_names), mesh_verts.shape[0], 3],
-                        requires_grad=False)
-                train_verts[i, ::] = mesh_verts
-
-            mean = torch.mean(train_verts, dim=0)
-            std = torch.std(train_verts, dim=0)
-            std = torch.where(std > 0, std, torch.tensor(1e-8))
-            normalization_dict = {'mean': mean, 'std': std}
-            torch.save(normalization_dict, normalization_dict_path)
-        return normalization_dict
-
-    def process(self):
-        for i, fname in tqdm.tqdm(enumerate(self.raw_file_names)):
-            mesh_verts = self.load_mesh(fname)
-
-            if self._normalize:
-                mesh_verts = (mesh_verts - self.mean) / self.std
-
-            data = Data(x=mesh_verts)
-
-            if self.pre_transform is not None:
-                data = self.pre_transform(data)
-            torch.save(data, os.path.join(self.processed_dir,
-                                          fname[:-4] + '.pt'))
-
-    def get(self, idx):
-        filename = self.raw_file_names[idx]
-        return torch.load(os.path.join(self.processed_dir,
-                                       filename[:-4] + '.pt'))
-
-    def len(self):
-        return len(self.processed_file_names)
-
-    def save_mean_mesh(self):
-        first_mesh_path = os.path.join(self._root, self._train_names[0])
-        first_mesh = trimesh.load_mesh(first_mesh_path, process=False)
-        first_mesh.vertices = self.mean.detach().cpu().numpy()
-        first_mesh.export(
-            os.path.join(self._precomputed_storage_path, 'mean.ply'))
-
-
-class MeshInMemoryDataset(InMemoryDataset):
-    def __init__(self, root, precomputed_storage_path='precomputed',
-                 dataset_type='train', normalize=True,
-                 transform=None, pre_transform=None, template=None):
-        self._root = root
-        self._precomputed_storage_path = precomputed_storage_path
-        if not os.path.isdir(precomputed_storage_path):
-            os.mkdir(precomputed_storage_path)
-
-        self._dataset_type = dataset_type
-        self._normalize = normalize
-        self._template = template
-
-        self._train_names, self._test_names, self._val_names = self.split_data(
-            os.path.join(precomputed_storage_path, 'data_split.json'))
-
-        normalization_dict = self.compute_mean_and_std()
-        self._normalization_dict = normalization_dict
-        self.mean = normalization_dict['mean']
-        self.std = normalization_dict['std']
-
-        super(MeshInMemoryDataset, self).__init__(
-            root, transform, pre_transform)
-
-        if dataset_type == 'train':
-            data_path = self.processed_paths[0]
-        elif dataset_type == 'test':
-            data_path = self.processed_paths[1]
-        elif dataset_type == 'val':
-            data_path = self.processed_paths[2]
-        else:
-            raise Exception("train, val and test are supported data types")
-
-        self.data, self.slices = torch.load(data_path)
-        if self.transform:
-            self.data = [self.transform(td) for td in self.data]
-
-    @property
-    def raw_file_names(self):
-        return 'mesh_data.zip'
-
-    @property
-    def processed_file_names(self):
-        return ['training.pt', 'test.pt', 'val.pt']
-
-    @property
-    def normalization_dict(self):
-        return self._normalization_dict
-
-    def download(self):
-        pass
 
     def find_filenames(self):
         root_l = len(self._root)
@@ -383,7 +218,7 @@ class MeshInMemoryDataset(InMemoryDataset):
         mesh_path = os.path.join(self._root, filename)
         mesh = trimesh.load_mesh(mesh_path, process=False)
         mesh_verts = torch.tensor(mesh.vertices, dtype=torch.float,
-                                  requires_grad=False)  # * scale (60 for LYHM)
+                                  requires_grad=False)
         if show:
             tm = trimesh.Trimesh(vertices=mesh.vertices,
                                  faces=self._template.face.t().cpu().numpy())
@@ -413,6 +248,103 @@ class MeshInMemoryDataset(InMemoryDataset):
             torch.save(normalization_dict, normalization_dict_path)
         return normalization_dict
 
+    def save_mean_mesh(self):
+        first_mesh_path = os.path.join(self._root, self._train_names[0])
+        first_mesh = trimesh.load_mesh(first_mesh_path, process=False)
+        first_mesh.vertices = self.mean.detach().cpu().numpy()
+        first_mesh.export(
+            os.path.join(self._precomputed_storage_path, 'mean.ply'))
+
+
+class MeshDataset(MixinMeshDataset, Dataset):
+    def __init__(self, root, precomputed_storage_path='precomputed',
+                 dataset_type='train', normalize=True,
+                 transform=None, pre_transform=None, template=None):
+
+        self._initialize(root, precomputed_storage_path,
+                         dataset_type, normalize, template)
+        super(MeshDataset, self).__init__(root, transform, pre_transform)
+
+    @property
+    def raw_file_names(self):
+        if self._dataset_type == 'train':
+            file_names = self._train_names
+        elif self._dataset_type == 'test':
+            file_names = self._test_names
+        elif self._dataset_type == 'val':
+            file_names = self._val_names
+        else:
+            raise Exception("train, val and test are supported dataset types")
+        return file_names
+
+    @property
+    def processed_file_names(self):
+        return self._processed_files
+
+    def download(self):
+        pass
+
+    def process(self):
+        for i, fname in tqdm.tqdm(enumerate(self.raw_file_names)):
+            mesh_verts = self.load_mesh(fname)
+
+            if self._normalize:
+                mesh_verts = (mesh_verts - self.mean) / self.std
+
+            data = Data(x=mesh_verts)
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            fname_split = fname.split(os.sep)
+            if len(fname_split) > 1:
+                fname = '_'.join(fname_split)
+            torch.save(data, os.path.join(self.processed_dir,
+                                          fname[:-4] + '.pt'))
+
+    def get(self, idx):
+        filename = self.raw_file_names[idx]
+        return torch.load(os.path.join(self.processed_dir,
+                                       filename[:-4] + '.pt'))
+
+    def len(self):
+        return len(self.processed_file_names)
+
+
+class MeshInMemoryDataset(MixinMeshDataset, InMemoryDataset):
+    def __init__(self, root, precomputed_storage_path='precomputed',
+                 dataset_type='train', normalize=True,
+                 transform=None, pre_transform=None, template=None):
+
+        self._initialize(root, precomputed_storage_path,
+                         dataset_type, normalize, template)
+        super(MeshInMemoryDataset, self).__init__(root, transform,
+                                                  pre_transform)
+
+        if dataset_type == 'train':
+            data_path = self.processed_paths[0]
+        elif dataset_type == 'test':
+            data_path = self.processed_paths[1]
+        elif dataset_type == 'val':
+            data_path = self.processed_paths[2]
+        else:
+            raise Exception("train, val and test are supported data types")
+
+        self.data, self.slices = torch.load(data_path)
+        if self.transform:
+            self.data = [self.transform(td) for td in self.data]
+
+    @property
+    def raw_file_names(self):
+        return 'mesh_data.zip'
+
+    @property
+    def processed_file_names(self):
+        return ['training.pt', 'test.pt', 'val.pt']
+
+    def download(self):
+        pass
+
     def _process_set(self, files_list):
         dataset = []
         for fname in tqdm.tqdm(files_list):
@@ -437,13 +369,6 @@ class MeshInMemoryDataset(InMemoryDataset):
         val_data = self._process_set(self._val_names)
         torch.save(self.collate(val_data), self.processed_paths[2])
 
-    def save_mean_mesh(self):
-        first_mesh_path = os.path.join(self._root, self._train_names[0])
-        first_mesh = trimesh.load_mesh(first_mesh_path, process=False)
-        first_mesh.vertices = self.mean.detach().cpu().numpy()
-        first_mesh.export(
-            os.path.join(self._precomputed_storage_path, 'mean.ply'))
-
 
 if __name__ == '__main__':
     import argparse
@@ -456,6 +381,4 @@ if __name__ == '__main__':
     opts = parser.parse_args()
     conf = utils.get_config(opts.config)
 
-    # BodyGenerator('/home/simo/Desktop').save_mean_mesh()
     tr_loader, val_loader, te_loader, norm_dict = get_data_loaders(conf, None)
-    tr_loader.dataset.save_mean_mesh()
